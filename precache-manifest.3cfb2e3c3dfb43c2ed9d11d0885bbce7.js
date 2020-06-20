8de34f2f7849536cde7445a31614fc4f4538cb09
Adrian-py <62276355+Adrian-py@users.noreply.github.com>
2020-06-20 22:03:12 +0700

    Updates

---
+++ precache-manifest.3cfb2e3c3dfb43c2ed9d11d0885bbce7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2834d83ac013518e1663a221f788c1c5",
+    "url": "/12MiaOne/index.html"
+  },
+  {
+    "revision": "4803c12f5efd69e6326f",
+    "url": "/12MiaOne/static/css/main.5c5cf2a4.chunk.css"
+  },
+  {
+    "revision": "03f7387c376c7f63f50d",
+    "url": "/12MiaOne/static/js/2.b37c8bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/12MiaOne/static/js/2.b37c8bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4803c12f5efd69e6326f",
+    "url": "/12MiaOne/static/js/main.d6487c87.chunk.js"
+  },
+  {
+    "revision": "316c94cd4cb9d8d3340a",
+    "url": "/12MiaOne/static/js/runtime-main.519322b2.js"
+  },
+  {
+    "revision": "f4c6c58f9f1a6e67ae3e1af569775b73",
+    "url": "/12MiaOne/static/media/Juanda.f4c6c58f.jpg"
+  },
+  {
+    "revision": "3d7e59a8dcd2bdcfe469e690f5260ea7",
+    "url": "/12MiaOne/static/media/bg.3d7e59a8.jpg"
+  }
+]);
